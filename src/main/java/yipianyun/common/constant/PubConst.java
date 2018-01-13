c6b288b9596f91de87325ddbb3c8b43c46c19e0e
fengmoge <youlangjx3@163.com>
2018-01-14 00:10:34 +0800

    新增defSDF

--- src/main/java/yipianyun/common/constant/PubConst.java
+++ src/main/java/yipianyun/common/constant/PubConst.java
@@ -3,6 +3,13 @@ package yipianyun.common.constant;
 import java.text.SimpleDateFormat;
 
 public class PubConst {
+	/**
+	 * yyyyMMddHHmmssSSS
+	 */
 	public static final SimpleDateFormat serialSDF=new SimpleDateFormat("yyyyMMddHHmmssSSS"); 
+	/**
+	 * yyyy-MM-dd HH:mm:ss
+	 */
 	public static final SimpleDateFormat defSDF=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); 
+	
 }
